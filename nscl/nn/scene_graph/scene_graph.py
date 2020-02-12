ac3400257cdf646602cb11b6746d366bf6288034
bergen <lbergen@grice.ucsd.edu>
2020-02-12 05:39:14 +0000

    update to subtractive rnn

--- nscl/nn/scene_graph/scene_graph.py
+++ nscl/nn/scene_graph/scene_graph.py
@@ -269,7 +269,66 @@ class MaxRNNSceneGraphBatched(NaiveRNNSceneGraphBatched):
         except Exception as e:
             self.subtractive_rnn = False
 
-    
+    def forward(self, input, objects, objects_length):
+        object_features = input
+        
+
+        batch_size = input.size(0)
+        max_num_objects = max(objects_length)
+       
+        outputs = list()
+        #object_features has shape batch_size x 256 x 16 x 24
+        obj_coord_map = torch.unsqueeze(coord_map((object_features.size(2),object_features.size(3)),object_features.device),dim=0)
+
+        obj_coord_map_batched = obj_coord_map.repeat(batch_size,1,1,1)
+
+        scene_object_coords_batched = torch.cat((object_features,obj_coord_map_batched), dim=1)
+
+        fused_object_coords_batched = self.object_coord_fuse(scene_object_coords_batched)
+
+        queries = self.get_queries(fused_object_coords_batched, batch_size, max_num_objects)
+
+        if not self.subtractive_rnn:
+            attention_map_batched = torch.einsum("bij,bjkl -> bikl", queries,fused_object_coords_batched)
+            attention_map_batched = nn.Softmax(2)(attention_map_batched.reshape(batch_size,max_num_objects,-1)).view_as(attention_map_batched)
+            object_values_batched = torch.einsum("bijk,bljk -> bil", attention_map_batched, fused_object_coords_batched) 
+            
+
+        else:
+            object_representations = []
+            remaining_scene = fused_object_coords_batched
+
+            
+            for query in queries:
+                attention_map_batched = torch.einsum("bj,bjkl -> bkl", query,remaining_scene)
+                attention_map_batched = nn.Softmax(1)(attention_map_batched.reshape(batch_size,-1)).view_as(attention_map_batched)
+                object_values = torch.einsum("bjk,bljk -> bl", attention_map_batched, remaining_scene) 
+                object_representations.append(object_values)
+
+                weighted_scene = torch.einsum("bjk,bljk -> bljk", attention_map_batched, remaining_scene) 
+
+                remaining_scene = remaining_scene - weighted_scene
+
+            object_values_batched = torch.stack(object_representations,dim=1)
+
+        object_representations_batched = self._norm(self.object_features_layer(object_values_batched))
+        object_pair_representations_batched = self.objects_to_pair_representations(object_representations_batched)
+
+
+        outputs = []
+        for i in range(batch_size):
+            num_objects = objects_length[i]
+            object_representations = torch.squeeze(object_representations_batched[i,0:num_objects,:],dim=0)
+            object_pair_representations = torch.squeeze(object_pair_representations_batched[i,0:num_objects,0:num_objects,:],dim=0).contiguous()
+            
+            outputs.append([
+                        None,
+                        object_representations,
+                        object_pair_representations
+                    ])
+
+
+        return outputs
 
     def get_queries(self,fused_object_coords,batch_size,max_num_objects):
 
@@ -297,7 +356,7 @@ class MaxRNNSceneGraphBatched(NaiveRNNSceneGraphBatched):
                 output, (h,c) = self.attention_rnn(scene_representation,(h,c))
 
                 query = output.view(batch_size,-1)
-                attention_map_batched = torch.einsum("bj,bjkl -> bkl", query,fused_object_coords)
+                attention_map_batched = torch.einsum("bj,bjkl -> bkl", query,remaining_scene)
                 attention_map_batched = nn.Softmax(1)(attention_map_batched.reshape(batch_size,-1)).view_as(attention_map_batched)
 
                 weighted_scene = torch.einsum("bjk,bljk -> bljk", attention_map_batched, remaining_scene) 
@@ -309,9 +368,9 @@ class MaxRNNSceneGraphBatched(NaiveRNNSceneGraphBatched):
 
 
 
-            queries = torch.stack(query_list,dim=1)
+            #queries = torch.stack(query_list,dim=1)
 
-            return queries
+            return query_list
 
 
     def test_batching(self,input, objects, objects_length):
